a50c6ab8161c1bd9dbb88f45c4cb36e8fe4190cd
mauriciogbarros <mauricio-gbarros@outlook.com>
2025-02-19 13:13:58 -0500

    Added my hotels view and e2e test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -33,10 +33,25 @@ test("Should allow user to add a hotel", async ({ page }) => {
 	await page.locator("[name='childCount']").fill("4");
 	await page.setInputFiles("[name='imageFiles']", [
 		path.join(__dirname, "files", "1.png"),
-		path.join(__dirname, "file", "2.png")
+		path.join(__dirname, "files", "2.png")
 	]);
 	await page.getByRole("button", { name: "Save" }).click();
 
 	await expect(page.getByText("Hotel Saved.")).toBeVisible();
 
-})
+});
+
+test("Should display hotels", async({ page }) => {
+	await page.goto(`${UI_URL}/my-hotels`);
+	
+	await expect(page.getByText("test01")).toBeVisible();
+	await expect(page.getByText("As they debated")).toBeVisible();
+	await expect(page.getByText("testCity, testCountry")).toBeVisible();
+	await expect(page.getByText("Boutique")).toBeVisible();
+	await expect(page.getByText("$200 per night")).toBeVisible();
+	await expect(page.getByText("3 adults, 4 children")).toBeVisible();
+	await expect(page.getByText("2 Star Rating")).toBeVisible();
+
+	await expect(page.getByRole("link", { name: "View Details" })).toBeVisible();
+	await expect(page.getByRole("link", { name: "Add Hotel" })).toBeVisible();
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 const App = () => {
 	const { isLoggedIn } = useAppContext();
@@ -43,14 +44,23 @@ const App = () => {
 					}
 				/>
 				{isLoggedIn && (<>
-					<Route
-						path="/add-hotel"
-						element = {
-							<Layout>
-								<AddHotel />
-							</Layout>
-						}
-					/></>
+						<Route
+							path="/add-hotel"
+							element = {
+								<Layout>
+									<AddHotel />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/my-hotels"
+							element = {
+								<Layout>
+									<MyHotels />
+								</Layout>
+							}
+						/>
+					</>
 				)}
 				<Route
 					path="*"
@@ -62,3 +72,4 @@ const App = () => {
 }
 
 export default App;
+// TODO: test my hotels route
